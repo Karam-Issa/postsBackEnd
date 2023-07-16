2ed2996f0286b6c76d0dacc815166e7617dd650e
karam <karamissa987@gmail.com>
2023-07-16 13:56:22 +0300

    new table in models for user info

--- models.py
+++ models.py
@@ -14,3 +14,9 @@ class Post(_database.Base):
     date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
 
 
+class userInformationPost(_database.Base):
+    __tablename__ = "userInformationPost"
+    id =_sql.Column(_sql.Integer, primary_key=True, index=True)
+    owner_id = _sql.Column(_sql.Integer, index=True)
+    first_name = _sql.Column(_sql.String,index=True)
+    last_name = _sql.Column(_sql.String,index=True)

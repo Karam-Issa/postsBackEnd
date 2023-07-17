9218188bfb10488f16a99e3aa1a099c3b9cc637d
karam <karamissa987@gmail.com>
2023-07-17 14:54:04 +0300

    New models adn schema for consuming from kafka topic

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ from database import Base
 
 
 class Post(_database.Base):
+    # Model for the Post table
     __tablename__ = "posts"
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
     owner_id = _sql.Column(_sql.Integer, index=True)
@@ -15,6 +16,7 @@ class Post(_database.Base):
 
 
 class userInformationPost(_database.Base):
+    # Model for the userInformationPost table
     __tablename__ = "userInformationPost"
     owner_id = _sql.Column(_sql.Integer,primary_key=True, index=True)
     first_name = _sql.Column(_sql.String,index=True)
--- schemas.py
+++ schemas.py
@@ -3,22 +3,27 @@
 import datetime as _dt
 import pydantic as _pydantic
 
+# Base schema for a post
 class _PostBase(_pydantic.BaseModel):
     post_text: str
 
+# Schema for creating a new post
 class PostCreate(_PostBase):
     pass
 
+# Schema for a post with additional fields
 class Post(_PostBase):
     id: int
     owner_id: int
     date_created: _dt.datetime
 
     class Config:
+        # Enable ORM mode to allow working with SQLAlchemy models
         orm_mode = True
 
-
+# Schema for the user producer
 class UserProducer(_pydantic.BaseModel):
     id : int
     first_name: str
     last_name :str
+

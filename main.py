9a0f4a5a2a44b9e17a3c2de21f2f0b63cc0ffe53
karam <karamissa987@gmail.com>
2023-07-17 14:53:16 +0300

    implemented producer to user stats about post count

--- main.py
+++ main.py
@@ -13,33 +13,27 @@ _JWT_SECRET = "thisisnotverysafe"
 
 app = _fastapi.FastAPI()
 
-
-
-
+# Create a background task to consume messages from Kafka
 asyncio.create_task(_services.consume())
 
 
 @app.post("/api/v1/createPost", response_model=_schemas.Post)
 async def create_post(post : _schemas.PostCreate, authorization: str = Header(), db: _orm.Session  = _fastapi.Depends(_services.get_db)):
-    
+    # Extract the JWT token from the Authorization header
     jwt_token = authorization.split("Bearer ")[1]
     
     try:
-       
+        # Decode the JWT token using the secret key
         decoded_token = jwt.decode(jwt_token, _JWT_SECRET, algorithms=["HS256"])
-
         
+        # Extract the user ID from the decoded token
         user_id = decoded_token["id"]
-
-        
-        
 
     except jwt.exceptions.DecodeError:
         raise _fastapi.HTTPException(
             status_code=401,
             detail="Invalid JWT token"
         )
-    
     
     return await _services.create_post(user_id=user_id, db=db, post=post)
 
@@ -48,23 +42,21 @@ async def create_post(post : _schemas.PostCreate, authorization: str = Header(),
 
 @app.get("/api/v1/getUserPosts", response_model=List[_schemas.Post])
 async def get_user_posts(authorization: str = Header(), db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    # Extract the JWT token from the Authorization header
     jwt_token = authorization.split("Bearer ")[1]
     
     try:
-       
+        # Decode the JWT token using the secret key
         decoded_token = jwt.decode(jwt_token, _JWT_SECRET, algorithms=["HS256"])
-
         
+        # Extract the user ID from the decoded token
         user_id = decoded_token["id"]
-
-        
-        
-
     except jwt.exceptions.DecodeError:
         raise _fastapi.HTTPException(
             status_code=401,
             detail="Invalid JWT token"
         )
+    
     return await _services.get_user_posts(user_id=user_id, db=db)       
 
 
--- services.py
+++ services.py
@@ -1,6 +1,3 @@
-
-#SERVICES STILL NOT ORGINIZED
-
 import database as _database
 import fastapi as _fastapi
 import models as _models
@@ -14,19 +11,13 @@ import email_validator as _email_val
 import jwt 
 # It provides password hashing and verification utilities.
 import passlib.hash as _hash
-
 from sqlalchemy.orm import sessionmaker
-
-Session = sessionmaker(bind=_database.engine)
-
-
-from config import loop, KAFKA_BOOTSTRAP_SERVERS,  KAFKA_CONSUMER_GROUP, KAFKA_TOPIC
+from config import loop, KAFKA_BOOTSTRAP_SERVERS,  KAFKA_CONSUMER_GROUP_POST, KAFKA_TOPIC_POST, KAFKA_TOPIC_USER, KAFKA_CONSUMER_GROUP_USER
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 import json
+
+
 _JWT_SECRET = "thisisnotverysafe"
-
-
-
 
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
@@ -34,15 +25,15 @@ if TYPE_CHECKING:
 
 # >>>import services
 # >>> services._add_tables()
-# it is a helper function responsible for creating the database tables based on the SQLAlchemy models defined in the database module.
+
 def _add_tables():
+    # Helper function responsible for creating the database tables based on the SQLAlchemy models defined in the database module.
     return _database.Base.metadata.create_all(bind=_database.engine)
 
 
-# It is a FastAPI dependency that provides a database session (db) to other routes and functions.
 def get_db():
+    # It is a FastAPI dependency that provides a database session (db) to other routes and functions.
     db = _database.SessionLocal()
-
     try:
         yield db
     finally:
@@ -52,10 +43,10 @@ def get_db():
 
 async def consume():
     # Obtain the database session or transaction object from the dependency injection system
-    
     db_session = _database.SessionLocal()
+
      # Create a Kafka consumer instance
-    consumer = AIOKafkaConsumer(KAFKA_TOPIC,loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS, group_id= KAFKA_CONSUMER_GROUP)
+    consumer = AIOKafkaConsumer(KAFKA_TOPIC_POST,loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS, group_id= KAFKA_CONSUMER_GROUP_POST)
     await consumer.start()
     try:
         # Consume messages from the Kafka topic
@@ -76,20 +67,44 @@ async def consume():
         await consumer.stop()
 
 
+
+
+async def produce_post_stats(owner_id: int, db: _orm.Session):
+    # Create a Kafka producer instance
+    producer = AIOKafkaProducer(loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+    await producer.start()
+    try:
+        post_dict = {
+            "owner_id": owner_id
+        }
+
+        # Convert the dictionary to a JSON string and encode as bytes
+        value_json = json.dumps(post_dict).encode('utf-8')
+
+        # Send the message to the Kafka topic
+        await producer.send_and_wait(topic=KAFKA_TOPIC_USER, value=value_json)
+    finally:
+        # Stop the producer to release resources
+        await producer.stop()
+
 async def create_post(user_id : str, db: _orm.Session, post: _schemas.PostCreate):
+    # Create a new post with the provided user_id and post data
     post = _models.Post(**post.dict(), owner_id = user_id)
     db.add(post)
     db.commit()
     db.refresh(post)
+    await produce_post_stats(user_id, db=db)
     return _schemas.Post.from_orm(post)
 
 
 async def get_user_posts(user_id: str, db: _orm.Session):
+    # Get all posts belonging to the specified user_id
     posts = db.query(_models.Post).filter_by(owner_id=user_id)
 
     return list(map(_schemas.Post.from_orm, posts))
 
 
 async def get_all_posts(db: _orm.Session):
+    # Get all posts from the database
     posts = db.query(_models.Post)
     return list(map(_schemas.Post.from_orm, posts))
